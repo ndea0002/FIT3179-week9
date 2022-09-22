40600ada5c2c040ea67f5c8a614fb87a6652ed52
ndea0002 <113740883+ndea0002@users.noreply.github.com>
2022-09-23 01:26:40 +0800

    Adding the java script file

---
+++ vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "JS/multi4.vg.json";
+
+vegaEmbed("#chart_1", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
